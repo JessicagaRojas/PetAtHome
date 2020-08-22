e04806c157c115fa5ecf73f26ba1cd734b2d32f7
JessicagaRojas <65513403+JessicagaRojas@users.noreply.github.com>
2020-08-22 12:06:15 +0200

    [backend] authentication working

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,10 +2,12 @@
 
 let express = require('express');
 let UserController = require('../controllers/UserController');
+let middleware_auth = require('../middlewares/auth');
+
 let api = express.Router();
 
 api.get('/home', UserController.home);
-api.get('/pruebas', UserController.pruebas);
+api.get('/pruebas', middleware_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
