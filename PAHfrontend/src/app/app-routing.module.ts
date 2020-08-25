a55d87c8f8e8b7db845c2dc38862b29a7ca37c17
JessicagaRojas <65513403+JessicagaRojas@users.noreply.github.com>
2020-08-25 22:21:09 +0200

    [frontend] register + login component added

--- PAHfrontend/src/app/app-routing.module.ts
+++ PAHfrontend/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
-const routes: Routes = [];
+//Importo los componentes a los que les quiero hacer una página aparte. Los que NO SON FIJOS
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- PAHfrontend/src/app/app.module.ts
+++ PAHfrontend/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import { routing } from './app-routing.module';
 import { AppRoutingModule } from './app-routing.module';
+
+
+// ----COMPONENTES----
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
