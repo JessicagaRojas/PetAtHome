28f9bedfb3e956212d662daca667eabb37035339
JessicagaRojas <65513403+JessicagaRojas@users.noreply.github.com>
2020-08-29 18:13:05 +0200

    [frontend] list all publications with paginator

--- PAHfrontend/src/app/components/timeline/timeline.component.ts
+++ PAHfrontend/src/app/components/timeline/timeline.component.ts
@@ -6,7 +6,10 @@ import { environment } from '../../../environments/environment';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
 import { Publication } from '../../models/publication';
+import { PublicationService } from '../../services/publication.service';
 
+
+ 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
@@ -17,12 +20,18 @@ export class TimelineComponent implements OnInit {
   public identity;
   public token;
   public users: User[];
+  public page;
+  public pages;
+  public total;
+
   public status: string;
+  public publications: Publication[];
 
   constructor(    private httpClient: HttpClient,
     private _route: ActivatedRoute,
     private _router: Router,
     private userService: UserService,
+    private publicationService: PublicationService
     
 
 
@@ -30,11 +39,41 @@ export class TimelineComponent implements OnInit {
     this.url = environment.url;
     this.identity = this.userService.getIdentity();
     this.token = this.userService.getToken();
+    this.page = 1;
     this.publication = this.publication;
 
    }
 
-  ngOnInit(): void {
+  ngOnInit(): void { this.getPublications(this.page); }
+
+
+   // --- MOSTRAR listado publicaciones (scroll) ----
+
+  getPublications(page){
+    this.publicationService.getPublications(this.token, page).subscribe(
+      response => {
+        if(response.publications){
+          this.total = response.total_items;
+          this.pages = response.pages;
+          this.publications = response.publications; //Dentro de esta propiedad está el array de publicaciones
+
+          if(page > this.pages){ //Si la págiuna actual es mayor a la que tengo guardada...
+            this._router.navigate(['/home']);  //...redirigimos a Home
+          }
+
+        }else{
+          this.status = 'error';
+        }
+      },
+      error => {
+        let errorMessage = <any>error;
+        console.log(errorMessage);
+
+        if(errorMessage != null){
+          this.status = 'error';
+        }
+      }
+    );
   }
 
   onSubmit() {}
--- PAHfrontend/src/app/services/publication.service.ts
+++ PAHfrontend/src/app/services/publication.service.ts
@@ -17,6 +17,8 @@ export class PublicationService {
 
    }
 
+   // ---- AÑADIR publicaciones
+
    addNewPublication(token, publication):Observable<any>{
      let params = JSON.stringify(publication); //Json convertido a string
      let headers = new HttpHeaders().set('Content-Type', 'application/json')
@@ -24,4 +26,27 @@ export class PublicationService {
       //Petición por POST al backend + guardado de estos parámetros
       return this.httpClient.post(this.url + 'publication', params, { headers: headers }); //Petición por POST al backend + guardado de estos parámetros
     }
+
+    //---- OBTENER publicaciones listada con paginator
+
+    getPublications(token, page = 1):Observable<any>{
+      let headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                      .set('Authorization', token);
+      //Petición por POST al backend + guardado de estos parámetros    
+
+      return this.httpClient.get(this.url + 'publications/' + page, { headers: headers }); //Petición por GET al backend + guardado de estos parámetros
+
+    }
+
+    //---- BORRAR publicaciones ----
+    
+
+    deletePublication(token, id):Observable<any>{
+      let headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                     .set('Authorization', token);
+      //Petición por POST al backend + guardado de estos parámetros    
+
+      return this.httpClient.delete(this.url + 'publications/' + id, { headers: headers }); //Petición por GET al backend + guardado de estos parámetros
+    }
+
 }
